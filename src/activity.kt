57e209b19b2dfaa0843c8faff2079902e02992ea
DarrenBlunk <darrenblunk@gmail.com>
2017-07-24 02:36:33 +0530

    Added Addition Function
    Signed-off-by: DarrenBlunk <darrenblunk@gmail.com>

--- src/activity.kt
+++ src/activity.kt
@@ -1,6 +1,19 @@
 /**
- * Created by bhara on 7/24/2017.
- */
+* Created by bharadwaj on 7/24/2017.
+*/
 fun main(args: Array<String>) {
+
+    //Getting the input from the console and converting it to int.
+    val a  =  readLine()!!.toInt()
+
+    println(addition(5,a))
+}
+
+fun HelloWorld(){
     println("Hello, World")
+}
+
+fun addition(a:Int, b:Int) : Int{
+    val c  = a + b
+    return c
 }
