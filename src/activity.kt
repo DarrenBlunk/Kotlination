6c7027f8b1c7e04902eb3ad5e3247bb116d2212e
DarrenBlunk <darrenblunk@gmail.com>
2017-07-24 16:13:03 +0530

    Refactored the code and added a display list for optimal reach to the various functions using user input
    Signed-off-by: DarrenBlunk <darrenblunk@gmail.com>

--- src/activity.kt
+++ src/activity.kt
@@ -3,20 +3,38 @@
 */
 fun main(args: Array<String>) {
 
-
-    //HelloWorld()
-
-    //Getting the input from the console and converting it to int.
-    /*val a  =  readLine()!!.toInt()
-    val b  =  readLine()!!.toInt()
-    println(addition(a,b))*/
-
-    //Reads Line and Reverses the string.
-    val word = readLine()
-    println(ManipulatorString.reverse(word!!))
-
+    Display()
+    Chooser(readLine()!!.toInt())
+}
+fun Display(){
+    val line = "*******************************"
+    val text = "Please Enter the number of the option you want to choose.\n " +
+            "1) Hello World \n 2) Addition \n 3) Reverse \n 4)More"
+    println(line + "\n" + text + "\n" + line)
 }
 
+fun Chooser(option :Int){
+    when (option) {
+            //Prints Hello World
+        1 -> HelloWorld()
+            //Adding to Numbers
+        2 -> {
+            //Getting the input from the console and converting it to int.
+            val a  =  readLine()!!.toInt()
+            val b  =  readLine()!!.toInt()
+            println(addition(a,b))
+        }
+            //Reads Line and Reverses the string.
+        3 -> {
+             val word = readLine()
+             println(ManipulatorString.reverse(word!!))
+        }
+        4 -> print("More have to be added.")
+        else -> {
+            print("You choose an invalid number")
+        }
+    }
+}
 fun HelloWorld(){
     println("Hello, World")
 }
